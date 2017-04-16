55d09cdcbe420f54dc2f13d08297b1ce40a2e717
Rajat Garg <RajatGarg@fico.com>
2017-04-16 16:40:15 +0530

    Solution of Exercise 1.4

---
+++ 1.4.lisp
@@ -0,0 +1,8 @@
+; Exerxise 1.4
+
+(defun (a-plus-abs-b a b)
+    ((if(> b 0) + - )a b)
+)
+
+; The Procedure will add two numbers a and b into which the number a will be the actual number entered by user and number b will be the absolute value of that number. 
+; So, suppose if user enter -5 then it will add 5 and if user enters 5 then again it will add 5.
